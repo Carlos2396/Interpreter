4820e501eb08566ab068bf1e4f8856925ab2bcf6
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2019-04-22 18:03:47 -0500

    Post order travesal

--- syntaxTree.c
+++ syntaxTree.c
@@ -9,6 +9,7 @@
 treeNode* createTreeNode(Instructions ins, Type t, Value v, Node* tblNode, treeNode* l, treeNode* c, treeNode* r){
     treeNode* newN = (treeNode*)malloc(sizeof(treeNode*));
     newN->instruction = ins;
+    newN->val = v;
     newN->type = t;
     newN->symbolTableNode = tblNode;
     newN->left = l;
@@ -17,3 +18,94 @@ treeNode* createTreeNode(Instructions ins, Type t, Value v, Node* tblNode, treeN
     return newN;
 }
 
+void postOrder(treeNode* tRoot){
+    if(tRoot == NULL)
+        return;
+    postOrder(tRoot->left);
+    postOrder(tRoot->center);
+    postOrder(tRoot->right);
+    char* inst;
+    switch (tRoot->instruction)
+    {
+        case IPROGRAM:
+            inst = "program";
+            break;
+        case IID:
+            inst = tRoot->symbolTableNode->identifier;
+            break;
+        case IBEGIN:
+            inst = "begin";
+            break;
+        case IEND:
+            inst = "end";
+            break;
+        case ILET:
+            inst = "let";
+            break;
+        case IINTEGER:
+            inst = "integer";
+            break;
+        case IREAL:
+            inst = "real";
+            break;
+        case IIF:
+            inst = "if";
+            break;
+        case ITHEN:
+            inst = "then";
+            break;
+        case IELSE:
+            inst = "else";
+            break;
+        case IWHILE:
+            inst = "while";
+            break;
+        case IDO:
+            inst = "do";
+            break;
+        case IREAD:
+            inst = "read";
+            break;
+        case IPRINT:
+            inst = "print";
+            break;
+        case ISEMICOLON:
+            inst = ";";
+            break;
+        case IPLUS:
+            inst = "+";
+            break;
+        case IMINUS:
+            inst = "-";
+            break;
+        case IASTERISK:
+            inst = "*";
+            break;
+        case ISLASH:
+            inst = "/";
+            break;
+        case IPARENTHESIS:
+            inst = "(";
+            break;
+        case ICPARENTHESIS:
+            inst = ")";
+            break;
+        case IINTNUM:
+            sprintf(inst, "%d", tRoot->val->intV);
+            break;
+        case IREALNUM:
+            sprintf(inst, "%f", tRoot->val->realV);
+            break;
+        case ISMALLER:
+            inst = "<";
+            break;
+        case IBIGGER:
+            inst = ">";
+            break;
+        case IEQUAL:
+            inst = "=";
+            break;
+        default:
+            break;
+    }
+}

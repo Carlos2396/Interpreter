bbe0a1922e47ae532b19542c597248058f6902a1
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2019-05-13 13:18:24 -0500

    DEfinitions and executioner documentation

--- libs/definitions.h
+++ libs/definitions.h
@@ -41,11 +41,14 @@
  * Unions and Enums definitions
 */
 
+//Enum that represent the instruction to be executed in the exectree
 typedef enum {IPROGRAM, IFUNCTION, IIDENTIFIER, IBEGIN, IEND, ILET, IINTEGER, IREAL, IIF, ITHEN, IELSE, IWHILE, IDO, IREAD, IPRINT, ISEMICOLON, IPLUS, IMINUS, IASTERISK, ISLASH, IPARENTHESIS, ICPARENTHESIS, IINTNUM, IREALNUM, ISMALLER, IBIGGER, IEQUAL, ISMALLEROREQUAL, IBIGGEROREQUAL, IDOT, ICOLON, IBRACKET, ICBRACKET, IASSIGNMENT, IRETURN} 
     Instruction;
 
+//Enum that represent the type of an expr, null is for the tree nodes that aren't an expr, term or factor.
 typedef enum {integer, real, null} Type;
 
+//Union to store both ints and floats 
 typedef union {
     int intV;
     float realV;
@@ -54,16 +57,13 @@ typedef union {
 /**
  * Structs definitions
 */
-typedef struct ArgNode ArgNode;
-typedef struct SymbolNode SymbolNode;
-typedef struct DeclNode DeclNode;
-typedef struct TreeNode TreeNode;
-typedef struct ParamNode ParamNode;
-typedef struct ArgNode ArgNode;
-typedef struct HashTable HashTable;
-typedef struct FunctionSymbolNode FunctionSymbolNode;
-typedef struct FunctionStackNode FunctionStackNode;
-typedef struct FunctionStack FunctionStack; 
+typedef struct ArgNode ArgNode; //Linked list to store arguments before insterting them into the symbol table
+typedef struct SymbolNode SymbolNode; //Linked list of sysmbols that are stored in the hash table
+typedef struct DeclNode DeclNode; //Linked list to store declarations before inserting them into the symbol table
+typedef struct TreeNode TreeNode; //Node of the execution tree, this node is multipourpuse that is wgy it has type, val, identifier and argList, every instruction can be represented with this node just nullyfy the attributes that are nit used.
+typedef struct ParamNode ParamNode; //Linked list to store declarations before inserting them into the symbol table and in the funtion symbol table
+typedef struct HashTable HashTable; //Struct that has both the hash table and its list of symbols to be inserted 
+typedef struct FunctionSymbolNode FunctionSymbolNode; //Hash table for the functions
 
 struct SymbolNode {
     char*identifier;
@@ -113,17 +113,6 @@ struct FunctionSymbolNode {
     struct FunctionSymbolNode*next;
 };
 
-struct FunctionStackNode {
-    char* identifier;
-    Value returnVal;
-    struct FunctionStackNode* next;
-};
-
-struct FunctionStack {
-    struct FunctionStackNode* head;
-    struct FunctionStackNode* tail;
-};
-
 
 /**
  * Methods
@@ -134,10 +123,10 @@ int hash(char*c); // makes simple hash with the identifier string
 /**
  *  Global Variables 
 */
-SymbolNode**globalTable;
-SymbolNode**currentTable;
-FunctionSymbolNode*currentFunction;
-Value returnValue;
+SymbolNode**globalTable; //The main symbol table with global variables, when using a variable the second thing to do is search it here
+SymbolNode**currentTable; //Local table to be used in the functions, when using a variable the first thing to do is search it here
+FunctionSymbolNode*currentFunction; //The node od the function hash table that is currently being used, this to make the declaration of parameters easier
+Value returnValue; //Value that is going to be returned in the function that is currently being executed
 extern int lines; // line counter
 extern FILE* yyin; // input file
 
--- libs/executioner.h
+++ libs/executioner.h
@@ -8,26 +8,25 @@
 /*
  * Methods
 */
-void handleError(int code, char*message);
 
-void execFunctionFunction(TreeNode* functNode, SymbolNode** hashTable);
-int evalExprInt(TreeNode* exprNode, SymbolNode** hashTable);
-int evalTermInt(TreeNode* termNode, SymbolNode** hashTable);
-int evalFactorInt(TreeNode* factorNode, SymbolNode** hashTable);
-float evalExprFloat(TreeNode* exprNode, SymbolNode** hashTable);
-float evalTermFloat(TreeNode* termNode, SymbolNode** hashTable);
-float evalFactorFloat(TreeNode* factorNode, SymbolNode** hashTable);
-int evalExpression(TreeNode* expressionNode, SymbolNode** hashTable);
-void ifFunction(TreeNode* ifNode, SymbolNode** hashTable);
-void thenFunction(TreeNode* thenNode, SymbolNode** hashTable);
-void elseFunction(TreeNode* elseNode, SymbolNode** hashTable);
-void whileFunction(TreeNode* whileNode, SymbolNode** hashTable);
-void doFunction(TreeNode* doNode, SymbolNode** hashTable);
-void printFunction(TreeNode* printNode, SymbolNode** hashTable);
-void assignFunction(TreeNode* assignNode, SymbolNode** hashTable);
-void beginFunction(TreeNode* beginNode, SymbolNode** hashTable);
-void execFunction(TreeNode* functNode, SymbolNode** hashTable);
-void execTree(TreeNode* root, SymbolNode** hashTable);
+void handleError(int code, char*message);//Method that prints errors and stops execution
+void execFunctionFunction(TreeNode* functNode, SymbolNode** hashTable);//Method that gets the identifier of a function, finds it in the table, copies its symbol table and asigns the arguments to the parameters and starts the execution of this function
+int evalExprInt(TreeNode* exprNode, SymbolNode** hashTable); //Funtion that gets the left exprand the right term and evaluates the expr, only for ints
+int evalTermInt(TreeNode* termNode, SymbolNode** hashTable);//Function that gets the left term and the right factor and evaluates the ter, only for ints
+int evalFactorInt(TreeNode* factorNode, SymbolNode** hashTable);//FUnction that returns the int value of the tree node, the symbol or the function to call
+float evalExprFloat(TreeNode* exprNode, SymbolNode** hashTable); //Funtion that gets the left exprand the right term and evaluates the expr, only for floats
+float evalTermFloat(TreeNode* termNode, SymbolNode** hashTable); //Function that gets the left term and the right factor and evaluates the ter, only for floats
+float evalFactorFloat(TreeNode* factorNode, SymbolNode** hashTable); //FUnction that returns the real value of the tree node, the symbol or the function to call
+int evalExpression(TreeNode* expressionNode, SymbolNode** hashTable); //Method that checks the type of the node, evaluates the left and the right expr and aplies the propper operation, returns 1 if true, 0 if false
+void ifFunction(TreeNode* ifNode, SymbolNode** hashTable);//Method that evaluates the expresion in its left child and it it is true executes the then node, if not and there is an else node executes the else node
+void thenFunction(TreeNode* thenNode, SymbolNode** hashTable);//Method that executes its left child
+void elseFunction(TreeNode* elseNode, SymbolNode** hashTable);//Method that, if its not null, executes its left child
+void whileFunction(TreeNode* whileNode, SymbolNode** hashTable);//Method that evaluates its left child (expresion) if true executes the do node
+void doFunction(TreeNode* doNode, SymbolNode** hashTable); //Method that executes its left child and then executes its while node
+void printFunction(TreeNode* printNode, SymbolNode** hashTable);//Method that evaluates its expr node and prints it
+void assignFunction(TreeNode* assignNode, SymbolNode** hashTable);//Method that evaluates its right node and modifies the symbol node that corresponds to its left node with the result
+void beginFunction(TreeNode* beginNode, SymbolNode** hashTable);//Method that executes its left child
+void execTree(TreeNode* root, SymbolNode** hashTable); //Method that checks the node instruction and calls the corresponding method
 
 
 #endif
